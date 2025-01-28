98148241ee2d8cbd7a0f05a463bc9f1c45d94c0f
mracsys <mracsys@gmail.com>
2025-01-27 19:55:12 -0500

    move en_ko asm hooks to separate file

--- ASM/src/hacks.asm
+++ ASM/src/hacks.asm
@@ -2599,23 +2599,6 @@ skip_bombchu_bowling_prize_switch:
 ;==================================================================================================
 ; Trade Quest Shuffle Flag Hooks
 ;==================================================================================================
-; Control if Fado (blonde Kokiri girl) can spawn in Lost Woods
-.headersize (0x80AD1C20 - 0xE51A60)
-.org 0x80AD3A84    ; Fado branch at end of EnKo_CanSpawn (called by EnKo_Init), override to always true
-    addiu   t5, $zero, 0x0031
-.org 0x80AD3EB0    ; end of EnKo_Init, controls Fado spawn
-    jal     check_fado_spawn_flags
-    sw      t8, 0x0180(s0)
-    lw      $ra, 0x001C($sp)
-    lw      s0, 0x0018($sp)
-    jr      $ra
-    addiu   $sp, $sp, 0x0020
-.headersize 0
-
-; Fix Fado's text id when trading in the odd potion out of order
-.orga 0xE535E4
-    sh      t2, 0x010E(s0)
-
 ; Control if Grog can spawn in Lost Woods
 .orga 0xE20BC8
     jal     check_grog_spawn_flags
@@ -4163,6 +4146,7 @@ DemoEffect_DrawJewel_AfterHook:
 .include "hacks/ovl_bg_haka_tubo.asm"
 .include "hacks/ovl_bg_spot18_basket.asm"
 .include "hacks/ovl_en_dns.asm"
+.include "hacks/ovl_en_ko.asm"
 .include "hacks/ovl_en_kz.asm"
 .include "hacks/ovl_obj_mure3.asm"
 .include "hacks/z_parameter.asm"
