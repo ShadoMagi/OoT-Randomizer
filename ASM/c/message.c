09bc165e37a309eb2d97e1b76a2cb264a3237d32
Fenhl <fenhl@fenhl.net>
2025-02-20 03:13:59 +0000

    Reintroduce redundancy

--- ASM/c/message.c
+++ ASM/c/message.c
@@ -237,7 +237,9 @@ uint8_t treasure_chest_game_show_message = 0;
 // Displays a warning message if the player attempted the Treasure Chest Game without
 // Lens of Truth when settings require it.
 void treasure_chest_game_message() {
-    treasure_chest_game_show_message = 1;
+    if (z64_file.items[Z64_SLOT_LENS] != Z64_ITEM_LENS || !z64_file.magic_acquired) {
+        treasure_chest_game_show_message = 1;
+    }
 }
 
 // Function to display custom textboxes ingame.
